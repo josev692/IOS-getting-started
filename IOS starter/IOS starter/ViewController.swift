d7b560fc9c946271c552fd6f9f6b507beaa8db55
Unknown <josev692@gmail.com>
2018-10-27 16:49:07 -0400

    create xcode project

---
+++ IOS starter/IOS starter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOS starter
+//
+//  Created by Jose Vargas on 10/27/18.
+//  Copyright © 2018 Jose Vargas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
